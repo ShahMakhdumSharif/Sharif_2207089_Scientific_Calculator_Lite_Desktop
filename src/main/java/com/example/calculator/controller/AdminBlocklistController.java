7b5c49cb65d04ecd4d7b93dca791287f5cc93077
ShahMakhdumShairf <shahmsharif2003@gmail.com>
2025-12-27 13:19:19 +0600

    Adding the feature of Block List view in Admin.fxml

--- src/main/java/com/example/calculator/controller/AdminBlocklistController.java
+++ src/main/java/com/example/calculator/controller/AdminBlocklistController.java
@@ -3,29 +3,28 @@ package com.example.calculator.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AdminController {
-
+public class AdminBlocklistController implements Initializable {
     @FXML
-    private Button AdminLogoutButton;
-
+    private Button AdminBlocklistBackButton;
     @FXML
-    private Button AdminAllUserButton;
-    @FXML
-    private Button AdminBlocklistButton;
+    private Button AdminBlocklistLogoutButton;
 
     @FXML
     protected void handleLogout(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/calculator/login.fxml"));
             Parent root = loader.load();
-            Stage stage = (Stage) AdminLogoutButton.getScene().getWindow();
+            Stage stage = (Stage) AdminBlocklistLogoutButton.getScene().getWindow();
             Scene currentScene = stage.getScene();
             if (currentScene != null) {
                 currentScene.setRoot(root);
@@ -41,11 +40,11 @@ public class AdminController {
     }
 
     @FXML
-    protected void handleAllUsers(ActionEvent event) {
+    protected void handleBack(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/calculator/AdminToUserInfo.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/calculator/Admin.fxml"));
             Parent root = loader.load();
-            Stage stage = (Stage) AdminAllUserButton.getScene().getWindow();
+            Stage stage = (Stage) AdminBlocklistBackButton.getScene().getWindow();
             Scene currentScene = stage.getScene();
             if (currentScene != null) {
                 currentScene.setRoot(root);
@@ -53,29 +52,15 @@ public class AdminController {
                 Scene scene = new Scene(root);
                 stage.setScene(scene);
             }
-            stage.setTitle("Admin - All Users");
+            stage.setTitle("Admin - Scientific Calculator Lite");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-     @FXML
-    protected void handleBlocklist(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/calculator/AdminBlocklist.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) AdminAllUserButton.getScene().getWindow();
-            Scene currentScene = stage.getScene();
-            if (currentScene != null) {
-                currentScene.setRoot(root);
-            } else {
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-            }
-            stage.setTitle("Admin - Block List");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
